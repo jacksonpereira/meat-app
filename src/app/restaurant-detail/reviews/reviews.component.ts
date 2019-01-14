33c9e962d912cff970fa4e6781a2bff9aee42270
Jackson Pereira <jackson.silva@mutantbr.com>
2019-01-14 17:48:39 -0200

    Trabalhando com Pipes"

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-reviews',
   templateUrl: './reviews.component.html'
 })
 export class ReviewsComponent implements OnInit {
+  reviews: Observable<any>
 
-  constructor() { }
+  constructor(private restautantsService: RestaurantsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews = this.restautantsService.reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
